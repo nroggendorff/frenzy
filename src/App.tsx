179c966994863b74043d4a83422e4e7b5e649aad
nroggendorff <nroggendorff87@gmail.com>
2024-04-23 17:46:48 -0500

    sync colors

--- src/App.tsx
+++ src/App.tsx
@@ -218,22 +218,20 @@ function ChatRoom() {
       <form onSubmit={sendMessage}>
         <label className="uploadFile">
           {isTyping ? (
-            <div className="arrow-icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-chevron-right"
-              >
-                <polyline points="9 18 15 12 9 6"></polyline>
-              </svg>
-            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="feather feather-chevron-right"
+            >
+              <polyline points="9 18 15 12 9 6"></polyline>
+            </svg>
           ) : (
             <svg
               xmlns="http://www.w3.org/2000/svg"
