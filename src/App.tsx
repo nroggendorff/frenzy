3c695962cc54237cd386482b33ee5c97568ca048
Noa <164402463+nroggendorff@users.noreply.github.com>
2024-04-23 20:16:48 -0500

    updatethebuttoninoneword

--- src/App.tsx
+++ src/App.tsx
@@ -217,9 +217,7 @@ function ChatRoom() {
         <div ref={dummy}></div>
       </main>
       <form onSubmit={sendMessage}>
-        <label className="uploadFile">
-          {isTyping ? (
-            <svg
+        {isTyping && (<label className="uploadFile"><svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
@@ -232,32 +230,36 @@ function ChatRoom() {
               className="feather feather-chevron-right"
             >
               <polyline points="9 18 15 12 9 6"></polyline>
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="feather feather-upload"
-            >
-              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-              <polyline points="17 8 12 3 7 8"></polyline>
-              <line x1="12" y1="3" x2="12" y2="15"></line>
-            </svg>
-          )}
-          <input
-            type="file"
-            onChange={handleFileChange}
-            accept="image/*"
-            style={{ display: "none" }}
-          />
+            </svg><button
+              onClick={() => setIsTyping(!isTyping)}
+              style={{ display: "none" }}
+            /></label>)}
+        {!isTyping && (
+            <label className="uploadFile">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="feather feather-upload"
+              >
+                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                <polyline points="17 8 12 3 7 8"></polyline>
+                <line x1="12" y1="3" x2="12" y2="15"></line>
+              </svg>
+            <input
+              type="file"
+              onChange={handleFileChange}
+              accept="image/*"
+              style={{ display: "none" }}
+            />
         </label>
+          )}
         <input
           value={formValue}
           onChange={(e) => {
