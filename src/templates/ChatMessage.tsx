817ea443ef578bce56d29c4c1ccfc3cf24390bb7
nroggendorff <nroggendorff87@gmail.com>
2024-05-12 13:01:11 -0500

    *sigh*

--- src/templates/ChatMessage.tsx
+++ src/templates/ChatMessage.tsx
@@ -79,6 +79,27 @@ function ChatMessage(props: {
             )}
           </td>
           <td>{text && <p className="message-content">{text}</p>}</td>
+          {messageClass !== "sent" && (
+            <td>
+              <button className="reply">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-corner-up-left"
+                >
+                  <polyline points="9 14 4 9 9 4"></polyline>
+                  <path d="M20 20v-7a4 4 0 0 0-4-4H4"></path>
+                </svg>
+              </button>
+            </td>
+          )}
         </tr>
         <tr>
           <td>
